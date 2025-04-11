aadc2ee0727256b0d1a8701f34a2452df998bc54
Hafiz011 <hrhafij8@gmail.com>
2025-04-11 22:59:42 +0600

    Decices API

---
+++ Services/DeviceAPI/DbContext/MongoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using DeviceAPI.Models;
+using MongoDB.Driver;
+namespace DeviceAPI.DbContext
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<Device> Devices => _database.GetCollection<Device>("Devices");
+        //public IMongoCollection<ApplicationUser> Users => _database.GetCollection<ApplicationUser>("Users");
+        //public IMongoCollection<ApplicationRole> Roles => _database.GetCollection<ApplicationRole>("Roles");
+
+        //public IMongoCollection<GeolocationModel> UserGeolocation => _database.GetCollection<GeolocationModel>("User_Geolocation");
+        //public IMongoCollection<ActivityLogsModel> UserLogs => _database.GetCollection<ActivityLogsModel>("User_Logs");
+
+
+    }
+
+}
---
+++ Services/DeviceAPI/Models/Device.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DeviceAPI.Models
+{
+    public class Device
+    {
+
+        public string Id { get; set; }
+        public string MACAddress { get; set; }
+        public string IPAddress { get; set; }
+        public string DeviceName { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
+
+    }
+}
